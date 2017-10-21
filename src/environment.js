4a67780c1d1e51f3405523f53418eedbd7ecfb20
Jaydeep Solanki <jaydp17@gmail.com>
2017-10-21 23:29:22 +0530

    add .env file

--- src/environment.js
+++ src/environment.js
@@ -1,3 +1,8 @@
+'use strict';
+
+// load environment variables from .env
+require('dotenv').config();
+
 exports.BASIC_AUTH = {
   username: process.env.BASIC_AUTH_USERNAME || 'someuser',
   password: process.env.BASIC_AUTH_PASSWORD || 'somepassword',
