37d28fefbe5c17ce6bbf53d92718e52e61f25220
Jaydeep Solanki <jaydp17@gmail.com>
2017-10-21 22:22:07 +0530

    add prettier

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000,  () => {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
 });
