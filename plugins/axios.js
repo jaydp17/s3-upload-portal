e3d80086eaeb4085cdb9b17d9f644d346efc2b46
Jaydeep Solanki <jaydp17@gmail.com>
2017-10-22 03:13:06 +0530

    add basic auth

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,6 +4,8 @@ const options = {};
 // The server-side needs a full url to works
 if (process.server) {
   options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`;
+  // eslint-disable-next-line global-require
+  options.auth = require('../server/utils/environment').BASIC_AUTH;
 }
 
 export default axios.create(options);
