f5c035e26db9bd1c78684da7b364eca7f7858650
Jaydeep Solanki <jaydp17@gmail.com>
2017-10-22 11:45:08 +0530

    basic footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
-  <footer>
-    Visit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Asset Uploader</strong> by <a href="https://jaydp.com" target="_blank">jaydp17</a>. 
+          The source code is licensed <a href="http://opensource.org/licenses/mit-license.php" target="_blank">MIT</a>.
+        </p>
+      </div>
+    </div>
   </footer>
 </template>
