4e053ac35734a1301ea708c9c0e0587b6d5830ee
Jaydeep Solanki <jaydp17@gmail.com>
2017-10-23 17:36:46 +0530

    update html meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'S3 Upload Portal',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { hid: 'description', name: 'description', content: packageJSON.description },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
